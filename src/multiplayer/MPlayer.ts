eb4d8fa2d57b0a6a9a47456b52bb20af7a33362b
alan.t.negrete1 <alan.t.negrete@gmail.com>
2018-12-07 17:26:05 -0600

    position updates

--- src/multiplayer/MPlayer.ts
+++ src/multiplayer/MPlayer.ts
@@ -53,53 +53,44 @@ export class MPlayer implements Engine {
       // this.room.listen('players/:position', (change: Colyseus.DataChange) => {
       //   console.log({ change });
       // });
-      this.room.listen(
-        'players/:id/"position/:x',
-        (change: Colyseus.DataChange) => {
-          if (self.room.sessionId != change.path.id) {
-            console.log(self.room.sessionId, change.value);
-            console.log({ change });
-            self.updateEnemeyPos(change.value, null, null);
-            let local = new Vector3(),
-              other = new Vector3();
-            this.tieFighter.getWorldPosition(local);
-            this.enemyTieFighter.getWorldPosition(other);
-            console.log(local, other);
-          }
+      this.room.listen('players/:id/:x', (change: Colyseus.DataChange) => {
+        if (self.room.sessionId != change.path.id) {
+          console.log(self.room.sessionId, change.value);
+          console.log({ change });
+          self.updateEnemeyPos(change.value, null, null);
+          let local = new Vector3(),
+            other = new Vector3();
+          this.tieFighter.getWorldPosition(local);
+          this.enemyTieFighter.getWorldPosition(other);
+          console.log(local, other);
         }
-      );
+      });
 
-      this.room.listen(
-        'players/:id/:position/:y',
-        (change: Colyseus.DataChange) => {
-          if (self.room.sessionId != change.path.id) {
-            console.log(self.room.sessionId, change.value);
-            console.log({ change });
-            self.updateEnemeyPos(null, change.value, null);
-            let local = new Vector3(),
-              other = new Vector3();
-            this.tieFighter.getWorldPosition(local);
-            this.enemyTieFighter.getWorldPosition(other);
-            console.log(local, other);
-          }
+      this.room.listen('players/:id/:y', (change: Colyseus.DataChange) => {
+        if (self.room.sessionId != change.path.id) {
+          console.log(self.room.sessionId, change.value);
+          console.log({ change });
+          self.updateEnemeyPos(null, change.value, null);
+          let local = new Vector3(),
+            other = new Vector3();
+          this.tieFighter.getWorldPosition(local);
+          this.enemyTieFighter.getWorldPosition(other);
+          console.log(local, other);
         }
-      );
+      });
 
-      this.room.listen(
-        'players/:id/:position/:z',
-        (change: Colyseus.DataChange) => {
-          if (self.room.sessionId != change.path.id) {
-            console.log(self.room.sessionId, change.value);
-            console.log({ change });
-            self.updateEnemeyPos(null, null, change.value);
-            let local = new Vector3(),
-              other = new Vector3();
-            this.tieFighter.getWorldPosition(local);
-            this.enemyTieFighter.getWorldPosition(other);
-            console.log(local, other);
-          }
+      this.room.listen('players/:id/:z', (change: Colyseus.DataChange) => {
+        if (self.room.sessionId != change.path.id) {
+          console.log(self.room.sessionId, change.value);
+          console.log({ change });
+          self.updateEnemeyPos(null, null, change.value);
+          let local = new Vector3(),
+            other = new Vector3();
+          this.tieFighter.getWorldPosition(local);
+          this.enemyTieFighter.getWorldPosition(other);
+          console.log(local, other);
         }
-      );
+      });
     });
   }
 
